a01c167b8cfffb10f5680ca1b0ad4cf0e3df7b55
srinigithub1 <40481341+srinigithub1@users.noreply.github.com>
2022-08-28 11:02:06 +0530

    commiting on 28 Aug

---
+++ AWSJenkinsSauceLabsAutomationCI/src/main/java/pkg_calculator/calculator.java
@@ -0,0 +1,35 @@
+package pkg_calculator;
+
+public class calculator {
+	
+	public int addition(int a, int b) {
+		int temp = a + b;
+		return temp;
+
+	}
+
+	public int Substraction(int a, int b) {
+
+		int temp = a - b;
+		return temp;
+	}
+
+	public int Multiplicaiton(int a, int b) {
+
+		int temp = a * b;
+		return temp;
+	}
+
+	protected int protectedMultiplicaiton(int a, int b) {
+
+		int temp = a * b;
+		return temp;
+	}
+
+	public int divistion(int a, int b) {
+
+		int temp = a / b;
+		return temp;
+	}
+
+}
